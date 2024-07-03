2e2fb82d14b2d71b4f450fabd2e7b54609480110
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-07-03 08:28:35 +0000

    first try

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 // Header.js
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
 
 const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 10px 20px;
+  padding: 20px;
   background-color: #f1f1f1;
   font-size: 24px;
   margin-bottom: 20px;
@@ -17,30 +19,10 @@ const Logo = styled.img`
   height: 50px;
 `;
 
-const NavMenu = styled.nav`
-  display: flex;
-  gap: 20px;
-`;
-
-const NavLink = styled(Link)`
-  text-decoration: none;
-  color: #333;
-  font-size: 18px;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
 const Header = () => (
   <StyledHeader>
     <Logo src="/2ul-top-logo.webp" alt="2ul.top Logo" />
     <div>2ul.top - URL Shortener</div>
-    <NavMenu>
-      <NavLink to="/pricing">Pricing</NavLink>
-      <NavLink to="/terms">Terms</NavLink>
-      <NavLink to="/privacy">Privacy Policy</NavLink>
-    </NavMenu>
   </StyledHeader>
 );
 
