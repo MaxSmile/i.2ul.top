0dc8b3934c741558061e37afbb8c3be9ae92fe9b
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-08-01 21:14:24 +0930

    ws

--- src/pages/[code].jsx
+++ src/pages/[code].jsx
@@ -5,13 +5,13 @@ import Layout from './_layout';
 
 export default function CodePage() {
     const router = useRouter();
-  const { code } = router.query;
+    const { code } = router.query;
     return (
         <Layout>
-        <div className="flex flex-col justify-center items-center min-h-screen bg-white">
-            
-            <p className="text-3xl font-bold">Code: {code}</p>
-        </div>
+            <div className="flex flex-col justify-center items-center min-h-screen bg-white">
+
+                <p className="text-3xl font-bold">Code: {code}</p>
+            </div>
         </Layout>
     );
 }
