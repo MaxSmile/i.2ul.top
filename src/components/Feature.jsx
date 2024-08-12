47944b5a43cbafa18f714183692d9827994f2faa
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-08-12 02:47:22 +0000

    testing replit

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,26 +1,10 @@
 // src/components/Feature.jsx
 import PropTypes from "prop-types";
-import { BiAbacus, BiLink, BiPen, BiQrScan } from "react-icons/bi";
 
-const icons = (idx) => {
-    const predef = {
-        "Pen": <BiPen />,
-        "Link": <BiLink />,
-        "QRCode": <BiQrScan />,
-    };
-    if (predef.hasOwnProperty(idx)) {
-        return predef[idx];
-    } else {
-        return <BiAbacus />;
-    }
-};
 const Feature = ({ title, desc, className }) => {
     return (
         <div className={`service group relative flex px-3 py-5 rounded-md duration-300 hover:bg-gray-50  ${className ?? ""}`}>
-            {/* Feature Icon */}
-            {/* <div className="mr-4 bg-white group-hover:bg-gray-50 relative flex-shrink-0 self-start w-[50px] md:w-auto">
-                {icons(thumb)}
-            </div> */}
+
 
             {/* Service Content */}
             <div>
