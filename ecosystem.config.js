60475dbc7f295e1fbb43bc2e175b2a08660cdae6
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-08-29 09:37:27 +0930

    fix of env vars

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,15 @@ module.exports = {
         max_memory_restart: '1G',
         instances: 1,
         env: {
+            NODE_ENV: 'development',
+            BASE_URL: 'http://localhost:3000',
+            CLIENT_URL: 'http://localhost:3002',
+            API_BASE_URL: 'http://localhost:3001',
+            API_READ_SHRTN_DATA: '/urls/',
+            API_WRITE_SHRTN_DATA: '/urls',
+            REDIRECT_DELAY_TIME: 15000
+          },
+          env_production: {
             NODE_ENV: 'production',
             BASE_URL: 'https://2ul.top',
             CLIENT_URL: 'https://client.2ul.top',
