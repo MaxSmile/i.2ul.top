ca0617b1d188d3156ead93dd5f5885f72ae23c63
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-10-11 17:17:27 +0930

    minors

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import Image from "next/image";
+import { BASE_URL } from '../data/constants';
 
 const Logo = ({className, chain=true}) => {
     return (
-        <Link href="/" className={`leading-0 inline-block ${className ?? ""}`}>
+        <Link href={BASE_URL} className={`leading-0 inline-block ${className ?? ""}`}>
                 <Image
                     alt="2UL.top logo"
                     width={60}
